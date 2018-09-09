5366677ecd5fd26aef854bfe4c0400abe75bfeea
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-09 19:13:38 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,10 @@ def third_challenge
       :hero => {
         name: "Romeo", age: "15", status: "alive" 
         },
-      :hero_friends => []
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
+      ]
    }, 
    :capulet => {
       :patriarch => {
